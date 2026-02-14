7433798a85ecaf1011145f7df0e4ad78079d181a
sienaindigoandlavender <siena@indigoandlavender.love>
2026-02-14 01:31:53 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel({
-    runtime: 'nodejs20.x',
-  }),
+  adapter: vercel(),
   site: 'https://moroccoartguide.com',
   build: {
     format: 'directory'
